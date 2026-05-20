fdeaec1681a2d0892d369dca63ebb7de915e0c8e
Boris Karabut <b03246599@gmail.com>
2026-05-20 16:38:36 +0300

    feat: Added header

---
+++ components/TheHeader.tsx
@@ -0,0 +1,11 @@
+import "@/styles/header.css";
+
+export default function TheHeader() {
+  return (
+    <header className="header">
+      <h1 className="header-title">
+        Swipe<span className="header-title-number">34</span>
+      </h1>
+    </header>
+  );
+}
